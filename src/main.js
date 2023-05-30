1d3aa3cb78422ade406209f94acca50f4d4462b5
geongupark <ggus.park@gmail.com>
2023-05-30 21:04:09 +0900

    Remove the old source

--- src/main.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue';
-import App from './App.vue'
-import router from './router';
-import VueKinesis from "vue-kinesis";
-import 'normalize.css';
-import 'bootstrap'
-import 'bootstrap/dist/css/bootstrap.min.css'
-
-createApp(App).use(router).use(VueKinesis).mount('#app')
