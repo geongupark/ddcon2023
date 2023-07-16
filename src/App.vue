eb338cb5c408325733f5d68225c971524714a268
geongupark <ggus.park@gmail.com>
2023-07-16 17:27:19 +0900

    Add for gsap

--- src/App.vue
+++ src/App.vue
@@ -1,49 +1,124 @@
 <script setup>
+import VideoPlay from "./components/hero/VideoPlay.vue";
 import Event from "./components/Event.vue";
 import axios from "axios";
 import { ref, onMounted, inject } from "vue";
-
+import gsap from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
 const $cookies = inject("$cookies");
 const visitorCount = ref(0);
 
-onMounted(async () => {
-  $cookies.config(60 * 60 * 12, "/", "192.168.219.105");
-  visitorCount.value = await getVisitorCount();
-});
+// onMounted(async () => {
+//   $cookies.config(60 * 60 * 12, "/", "192.168.219.105");
+//   visitorCount.value = await getVisitorCount();
+// });
 
-async function getVisitorCount() {
-  try {
-    const count = $cookies.get("visitorCount");
-    const response = await axios.get(
-      "http://192.168.219.105:8009/meta-int/visitor"
-    );
-    let visitorCount;
-    if (!count) {
-      $cookies.set("visitorCount", response.data["value"]);
-      visitorCount = await increaseVisitorCount();
-      return visitorCount.data["value"];
-    }
-    return response.data["value"];
-  } catch (error) {
-    console.error("Error:", error);
-  }
-}
-async function increaseVisitorCount() {
-  try {
-    const response = await axios.post(
-      "http://192.168.219.105:8009/meta-int/increase/visitor"
-    );
-    return response;
-  } catch (error) {
-    console.error("Error:", error);
-  }
-}
+// async function getVisitorCount() {
+//   try {
+//     const count = $cookies.get("visitorCount");
+//     const response = await axios.get(
+//       "http://192.168.219.105:8009/meta-int/visitor"
+//     );
+//     let visitorCount;
+//     if (!count) {
+//       $cookies.set("visitorCount", response.data["value"]);
+//       visitorCount = await increaseVisitorCount();
+//       return visitorCount.data["value"];
+//     }
+//     return response.data["value"];
+//   } catch (error) {
+//     console.error("Error:", error);
+//   }
+// }
+// async function increaseVisitorCount() {
+//   try {
+//     const response = await axios.post(
+//       "http://192.168.219.105:8009/meta-int/increase/visitor"
+//     );
+//     return response;
+//   } catch (error) {
+//     console.error("Error:", error);
+//   }
+// }
+onMounted(() => {
+  const ufo = gsap.timeline({
+    scrollTrigger: {
+      trigger: ".box1",
+      start: "top top",
+      end: "center",
+      markers: { startColor: "red", endColor: "red", fontSize: "12px" },
+      scrub: 1,
+      pin: true,
+    },
+  });
+  ufo.to(".ufo", {
+    height: 100,
+    rotate: -60,
+    xPercent: 300,
+    yPercent: 100,
+    duration: 1,
+  });
+  ufo.to(".ufo", {
+    height: 250,
+    rotate: 45,
+    // xPercent: 600,
+    yPercent: 100,
+    duration: 1,
+  });
+  ufo.to(".ufo", {
+    height: 500,
+    rotate: -45,
+    // xPercent: 900,
+    yPercent: -100,
+    duration: 1,
+  });
+
+  const astronaut = gsap.timeline({
+    scrollTrigger: {
+      trigger: ".box2",
+      start: "top top",
+      end: "center",
+      markers: { startColor: "purple", endColor: "purple", fontSize: "12px" },
+      scrub: 1,
+      pin: true,
+    },
+  });
+  astronaut.to(".astronaut", {
+    xPercent: 300,
+    yPercent: 100,
+    rotate: 30,
+    duration: 1,
+  });
+  astronaut.to(".astronaut", {
+    xPercent: 600,
+    yPercent: 100,
+    rotate: -30,
+    duration: 1,
+  });
+  astronaut.to(".astronaut", {
+    xPercent: 900,
+    yPercent: -100,
+    rotate: 60,
+    duration: 1,
+  });
+});
 </script>
 
 <template>
   <div class="conference-website bg-indigo-900">
+    <div class="w-full"></div>
+    <VideoPlay class="w-full"></VideoPlay>
     <div class="h-40">{{ visitorCount }}</div>
     <Event></Event>
+    <div class="box1 bg-red-200 relative h-[2000px]">
+      <img class="ufo absolute left-20 top-20" src="@/assets/ufo.png" />
+    </div>
+    <div class="box2 bg-red-200 relative h-[2000px]">
+      <img
+        class="astronaut h-28 absolute left-20 top-20"
+        src="@/assets/astronaut.png"
+      />
+    </div>
   </div>
 </template>
 
---
+++ src/components/hero/VideoPlay.vue
@@ -0,0 +1,44 @@
+<script setup>
+import { ref, onMounted } from "vue";
+import { gsap } from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
+const heroVideo = ref(null);
+const scrollHeight = ref("1100px");
+
+gsap.registerPlugin(ScrollTrigger);
+onMounted(() => {
+  heroVideo.value = document.querySelector(".hero-video");
+  gsap.to(".hero-video", {
+    scrollTrigger: {
+      trigger: ".hero-video-box",
+      toggleActions: "play none none reverse",
+      pin: true,
+      start: "top top-=1px",
+      end: `${scrollHeight.value} center`,
+      // markers: true,
+      onEnter: (self) => {
+        heroVideo.value.play();
+      },
+      onEnterBack: (self) => {
+        heroVideo.value.play();
+      },
+      onLeave: (self) => {
+        heroVideo.value.pause();
+      },
+      onLeaveBack: (self) => {
+        heroVideo.value.pause();
+      },
+    },
+  });
+});
+</script>
+<template>
+  <div class="hero-video-box">
+    <video
+      class="hero-video w-screen h-screen object-cover"
+      src="/public/main_session_animation.mp4"
+      loop
+      muted
+    ></video>
+  </div>
+</template>
